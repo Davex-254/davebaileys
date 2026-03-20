51e0699ba422fb68791b4db877d37427d2db1f67
Davex-254 <babydave081@gmail.com>
2026-03-20 03:23:11 +0300

    Dave x

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const baileys = require('./lib/index.js');
 
-console.log('=== davebaileys Library Test ===\n');
+console.log('=== gifted-baileys Library Test ===\n');
 console.log('Library loaded successfully!');
 console.log('\nAvailable exports:');
 console.log(Object.keys(baileys).join('\n'));
